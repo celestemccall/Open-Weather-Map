ff1fddc14178f8b24ade1e66fe36986f90cb698a
Celeste McCall <celestemccall93@gmail.com>
2023-03-05 01:34:09 -0600

    Inital Commit

---
+++ OpenWeatherMap/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json.Linq;
+using OpenWeatherMap;
+
+namespace OpenWeatherApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string key = File.ReadAllText("appsettings.json");
+            string APIKey = JObject.Parse(key).GetValue("APIKey").ToString();
+
+            Console.WriteLine("Please enter your zip code.");
+            var zipCode = Console.ReadLine();
+
+            var apiCall = $"https://api.openweathermap.org/data/2.5/weather?zip={zipCode}&appid={APIKey}&units=imperial";
+
+            Console.WriteLine("");
+
+            Console.WriteLine($"It is currently {WeatherMap.GetTemp(apiCall)} degrees Fahrenheit in your location.");
+
+
+        }
+    }
+}
